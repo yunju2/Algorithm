e2ef969236512d0a25a1773d2672f877cc4dced2
yunju2 <61370487+yunju2@users.noreply.github.com>
2022-06-21 03:19:35 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double num = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        double div = num/num2;
+
+        
+        System.out.println(div);
+
+    }
+}
