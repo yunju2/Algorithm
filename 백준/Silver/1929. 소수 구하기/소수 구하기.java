1f5941ec87564068abff91f8071414e4f6c2ae35
yunju2 <61370487+yunju2@users.noreply.github.com>
2022-11-12 00:43:10 +0900

    [Silver III] Title: 소수 구하기, Time: 308 ms, Memory: 22396 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+ 
+ 
+public class Main {
+	public static boolean[] prime;
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		int M = in.nextInt();
+		int N = in.nextInt();
+		
+		prime = new boolean[N + 1];
+		get_prime();
+				
+		StringBuilder sb = new StringBuilder();
+		for(int i = M; i <= N; i++) {
+			if(!prime[i]) sb.append(i).append('\n');
+		}
+		System.out.println(sb);
+	}
+ 
+
+	public static void get_prime() {
+		prime[0] = prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+	}
+}
+ 
+ 
