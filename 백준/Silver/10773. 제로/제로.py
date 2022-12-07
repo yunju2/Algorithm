bceed0206e1236cd2df23fb6c0dd19670155da7c
yunju2 <61370487+yunju2@users.noreply.github.com>
2022-12-07 23:34:03 +0900

    [Silver IV] Title: 제로, Time: 4000 ms, Memory: 31240 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+K = int(input())
+num_stack = []
+
+for _ in range(K):
+    num = int(input())
+    if num == 0:
+        del num_stack[-1]
+    else: 
+        num_stack.append(num)
+
+if not num_stack:
+    print(0)
+else:
+    print(sum(num_stack))
