431f0b84ee18457884dc4ed62226b8368d7d9873
yunju2 <61370487+yunju2@users.noreply.github.com>
2023-10-11 19:28:54 +0900

    [Gold III] Title: 줄 세우기, Time: 1320 ms, Memory: 165992 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 
@@ -35,8 +34,6 @@ public class Main {
         }
 
     }
-
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
 
